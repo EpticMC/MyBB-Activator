e905ab97d933171e877b8ada02a9d585a5dc8b54
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-16 11:00:47 +0200

    removed unused stuff

--- inc/plugins/activate.php
+++ inc/plugins/activate.php
@@ -24,22 +24,15 @@ function activate_activate(){ global $db, $cache; }
 function activate_deactivate(){ global $db, $cache; }
 
 function activate_run($profile){
-    global $db, $mybb, $cache;
-    verify_post_check($mybb->get_input('my_post_key'));
-    if ($mybb->usergroup['canactivateusers'] == 0) error_no_permission();
-    $mybb->input['check'] = $mybb->get_input('check', MyBB::INPUT_ARRAY);
-    if (empty($mybb->input['check'])) error("No user selcted");
-    $mybb->input['check'] = array_map('intval', $mybb->input['check']);
+    global $db, $cache;
     $user_ids = implode(", ", $profile);
-    if (empty($user_ids)) error("No user selcted");
-    if ($mybb->get_input('activate')){
-        $query = $db->simple_select("users", "uid, username, email, usergroup, coppauser", "uid IN ({$user_ids})");
-        while ($user = $db->fetch_array($query)){
-            if($user['coppauser']) $updated_user = array("coppauser" => 0);
-            else $db->delete_query("awaitingactivation", "uid='{$user['uid']}'");
-            if ($user['usergroup'] == 5) $updated_user['usergroup'] = 2;
-            $db->update_query("users", $updated_user, "uid='{$user['uid']}'");
-        }
+    if (empty($user_ids)) die("No user selcted");
+    $query = $db->simple_select("users", "uid, username, email, usergroup, coppauser", "uid IN ({$user_ids})");
+    while ($user = $db->fetch_array($query)){
+        if($user['coppauser']) $updated_user = array("coppauser" => 0);
+        else $db->delete_query("awaitingactivation", "uid='{$user['uid']}'");
+        if ($user['usergroup'] == 5) $updated_user['usergroup'] = 2;
+        $db->update_query("users", $updated_user, "uid='{$user['uid']}'");
     }
     $cache->update_awaitingactivation();
 }
