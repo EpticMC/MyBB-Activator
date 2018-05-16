91d4550c1d078343afbe6a2041f03134579d796b
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-16 10:57:37 +0200

    refs

--- inc/plugins/activate.php
+++ inc/plugins/activate.php
@@ -23,25 +23,25 @@ function activate_info(){
 function activate_activate(){ global $db, $cache; }
 function activate_deactivate(){ global $db, $cache; }
 
-function activate_run(){
+function activate_run($profile){
     global $db, $mybb, $cache;
-    if ($mybb->input['action'] == "do_activate"){
-        verify_post_check($mybb->get_input('my_post_key'));
-        if ($mybb->usergroup['canactivateusers'] == 0) error_no_permission();
-        $mybb->input['check'] = $mybb->get_input('check', MyBB::INPUT_ARRAY);
-        if (empty($mybb->input['check'])) error("No user selcted");
-        $mybb->input['check'] = array_map('intval', $mybb->input['check']);
-        $user_ids = implode(", ", $mybb->input['check']);
-        if (empty($user_ids)) error("No user selcted");
-        if ($mybb->get_input('activate')){
-            $query = $db->simple_select("users", "uid, username, email, usergroup, coppauser", "uid IN ({$user_ids})");
-            while ($user = $db->fetch_array($query)){
-                if($user['coppauser']) $updated_user = array("coppauser" => 0);
-                else $db->delete_query("awaitingactivation", "uid='{$user['uid']}'");
-                if ($user['usergroup'] == 5) $updated_user['usergroup'] = 2;
-                $db->update_query("users", $updated_user, "uid='{$user['uid']}'");
-            }
+    verify_post_check($mybb->get_input('my_post_key'));
+    if ($mybb->usergroup['canactivateusers'] == 0) error_no_permission();
+    $mybb->input['check'] = $mybb->get_input('check', MyBB::INPUT_ARRAY);
+    if (empty($mybb->input['check'])) error("No user selcted");
+    $mybb->input['check'] = array_map('intval', $mybb->input['check']);
+    $user_ids = implode(", ", $profile);
+    if (empty($user_ids)) error("No user selcted");
+    if ($mybb->get_input('activate')){
+        $query = $db->simple_select("users", "uid, username, email, usergroup, coppauser", "uid IN ({$user_ids})");
+        while ($user = $db->fetch_array($query)){
+            if($user['coppauser']) $updated_user = array("coppauser" => 0);
+            else $db->delete_query("awaitingactivation", "uid='{$user['uid']}'");
+            if ($user['usergroup'] == 5) $updated_user['usergroup'] = 2;
+            $db->update_query("users", $updated_user, "uid='{$user['uid']}'");
         }
-        $cache->update_awaitingactivation();
     }
+    $cache->update_awaitingactivation();
 }
+
+//activate_run(["test", "test"]);
