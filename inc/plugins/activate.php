d4284d1c6663f663a74ed5132fbbf0e7d104457b
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-24 09:50:59 +0200

    try to hook misc.php and wait for action

--- inc/plugins/activate.php
+++ inc/plugins/activate.php
@@ -7,6 +7,8 @@
 
 if (!defined("IN_MYBB")) die("Direct initialization prohibited. IN_MYBB is not defined.");
 
+$plugins->add_hook("misc_start", "handle_hook");
+
 function activate_info(){
     return array(
         "name"              => "EpticMC Activator",
@@ -23,18 +25,30 @@ function activate_info(){
 function activate_activate(){ global $db, $cache; }
 function activate_deactivate(){ global $db, $cache; }
 
-function activate_run($profile){
+function pillars_page() {
+    global $mybb, $db, $plugins;    
+    
+    if ($mybb->input["action"] != "activateuser") return;
+
+    //TODO:
+    //-> Register Code to UID
+    //profile_activation($uid);
+}
+
+function profile_activation($profile){
     global $db, $cache;
     $user_ids = implode(", ", $profile);
+
+
     if (empty($user_ids)) die("No user selcted");
     $query = $db->simple_select("users", "uid, username, email, usergroup, coppauser", "uid IN ({$user_ids})");
     while ($user = $db->fetch_array($query)){
-        if($user['coppauser']) $updated_user = array("coppauser" => 0);
+        if($user["coppauser"]) $updated_user = array("coppauser" => 0);
         else $db->delete_query("awaitingactivation", "uid='{$user['uid']}'");
-        if ($user['usergroup'] == 5) $updated_user['usergroup'] = 2;
+        if ($user["usergroup"] == 5) $updated_user['usergroup'] = 2;
         $db->update_query("users", $updated_user, "uid='{$user['uid']}'");
     }
     $cache->update_awaitingactivation();
 }
 
-//activate_run(["1", "2"]);
+
